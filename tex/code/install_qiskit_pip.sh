cfb9b5ff0163bde38324b2ca4508e089bb5cefef
Stuart Kingham <stuart_kingham@yahoo.com>
2024-05-28 11:05:07 +0100

    Finish theory sections and add appendices

--- tex/code/install_qiskit_pip.sh
+++ tex/code/install_qiskit_pip.sh
@@ -1,3 +1,4 @@
 python3 -m venv /path/to/virtual/environment
 source /path/to/virtual/environment/bin/activate
-pip install ipykernel qutip qiskit[visualization] qiskit-ibm-runtime
+pip install pandas numpy ipykernel qutip qiskit[visualization] \
+    qiskit-ibm-runtime
